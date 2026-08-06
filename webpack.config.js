cca19d3482429d09d6401c3010abbbe8ecf897db
Eugen Morozov <26843480+meugenom@users.noreply.github.com>
2026-08-06 13:25:34 +0200

    Configuration for proper routing operation

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,12 @@
-// webpack.config.js
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: isProd ? './' : '/',
+    publicPath: '/',
   },
   module: {
     rules: [
